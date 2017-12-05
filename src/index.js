666523009e14e6abba9190d6c0a892abc1d5006f
mrzmmr <mrzmmr@gmail.com>
2017-12-05 08:12:43 +0000

    add lint

--- src/index.js
+++ src/index.js
@@ -1,46 +1,48 @@
+'use strict'
+
 /**
  * Dependencies
  */
-const visit = require('unist-util-visit')
-const select = require('hast-util-select')
-const parseSelector = require('hast-util-parse-selector')
+const visit = require('unist-util-visit');
+const select = require('hast-util-select');
+const parseSelector = require('hast-util-parse-selector');
 
 /**
  * Attacher
  */
 module.exports = options => {
-  options = options || {}
-  const selector = options.selector || options.select || 'body'
-  const wrapper = options.wrapper || options.wrap
+  options = options || {};
+  const selector = options.selector || options.select || 'body';
+  const wrapper = options.wrapper || options.wrap;
 
   /**
    * Transformer
    */
   return tree => {
     if (typeof wrapper !== 'string') {
-      throw new TypeError('Expected a `string` as wrapper')
+      throw new TypeError('Expected a `string` as wrapper');
     }
 
     if (typeof selector !== 'string') {
-      throw new TypeError('Expected a `string` as selector')
+      throw new TypeError('Expected a `string` as selector');
     }
 
-    const selected = select.select(selector, tree)
-    const wrap = parseSelector(wrapper)
+    const selected = select.select(selector, tree);
+    const wrap = parseSelector(wrapper);
 
     if (selected) {
       visit(tree, selected, (node, i, parent) => {
         if (node.tagName === 'body') {
-          wrap.children = node.children
-          node.children = [wrap]
+          wrap.children = node.children;
+          node.children = [wrap];
         } else {
-          wrap.children = [selected]
-          parent.children[i] = wrap
+          wrap.children = [selected];
+          parent.children[i] = wrap;
         }
-      })
+      });
     } else {
-      wrap.children = tree.children
-      tree.children = [wrap]
+      wrap.children = tree.children;
+      tree.children = [wrap];
     }
-  }
-}
+  };
+};
--- src/test.js
+++ src/test.js
@@ -1,12 +1,12 @@
-'use strict'
+'use strict';
 
-const test = require('tap').test
-const rehype = require('rehype')
-const unified = require('unified')
-const remarkParse = require('remark-parse')
-const remarkRehype = require('remark-rehype')
-const rehypeStringify = require('rehype-stringify')
-const wrap = require('./')
+const test = require('tap').test;
+const rehype = require('rehype');
+const unified = require('unified');
+const remarkParse = require('remark-parse');
+const remarkRehype = require('remark-rehype');
+const rehypeStringify = require('rehype-stringify');
+const wrap = require('./');
 
 const markdown = `
 # Foo
@@ -14,7 +14,7 @@ const markdown = `
 \`\`\`js
 const foo = 'bar'
 \`\`\`
-`
+`;
 
 const process = (plugin, options, markdown) => {
   return unified()
@@ -22,80 +22,96 @@ const process = (plugin, options, markdown) => {
     .use(remarkRehype)
     .use(plugin, options)
     .use(rehypeStringify)
-    .processSync(markdown)
-}
+    .processSync(markdown);
+};
 
 test('rehype-wrap', t => {
-  let vfile
+  let vfile;
 
   t.test('should throw', it => {
-    it.throws(() => {
-      vfile = process(wrap, null, markdown)
-    }, /Expected a `string` as wrapper/,
-    ' if no options are given')
+    it.throws(
+      () => {
+        vfile = process(wrap, null, markdown);
+      },
+      /Expected a `string` as wrapper/,
+      ' if no options are given'
+    );
 
-    it.throws(() => {
-      vfile = process(wrap, {wrap: 'a', select: 1}, markdown)
-    }, /Expected a `string` as selector/,
-    ' if selector is not a string')
-    it.end()
-  })
+    it.throws(
+      () => {
+        vfile = process(wrap, { wrap: 'a', select: 1 }, markdown);
+      },
+      /Expected a `string` as selector/,
+      ' if selector is not a string'
+    );
+    it.end();
+  });
 
   t.test('should not throw', it => {
     it.doesNotThrow(() => {
-      vfile = process(wrap, {wrap: 'div'}, markdown)
+      vfile = process(wrap, { wrap: 'div' }, markdown);
 
-      it.ok(vfile.toString().length > 0, 'should process')
-      it.ok(vfile.toString() === [
-        '<div><h1>Foo</h1>',
-        '<pre><code class="language-js">const foo = \'bar\'',
-        '</code></pre></div>'
-      ].join('\n'),
+      it.ok(vfile.toString().length > 0, 'should process');
+      it.ok(
+        vfile.toString() ===
+          [
+            '<div><h1>Foo</h1>',
+            '<pre><code class="language-js">const foo = \'bar\'',
+            '</code></pre></div>'
+          ].join('\n'),
         'should be equal wrap div'
-      )
+      );
 
       vfile = unified()
         .use(remarkParse)
         .use(remarkRehype)
         .use(rehypeStringify)
         .processSync(markdown)
-        .toString()
+        .toString();
 
       const vfileWithBody = rehype()
-        .use(wrap, {wrap: 'div'})
+        .use(wrap, { wrap: 'div' })
         .processSync(vfile)
-        .toString()
+        .toString();
 
-      it.ok(vfileWithBody === [
-        '<html><head></head><body><div><h1>Foo</h1>',
-        '<pre><code class="language-js">const foo = \'bar\'',
-        '</code></pre></div></body></html>'
-      ].join('\n'),
-      'should be equal wrap div with body'
-      )
+      it.ok(
+        vfileWithBody ===
+          [
+            '<html><head></head><body><div><h1>Foo</h1>',
+            '<pre><code class="language-js">const foo = \'bar\'',
+            '</code></pre></div></body></html>'
+          ].join('\n'),
+        'should be equal wrap div with body'
+      );
 
-      vfile = process(wrap, {
-        select: 'pre',
-        wrap: 'div'
-      }, markdown)
+      vfile = process(
+        wrap,
+        {
+          select: 'pre',
+          wrap: 'div'
+        },
+        markdown
+      );
 
-      it.ok(vfile.toString() === [
-        '<h1>Foo</h1>',
-        '<div><pre><code class="language-js">const foo = \'bar\'',
-        '</code></pre></div>'
-      ].join('\n'),
+      it.ok(
+        vfile.toString() ===
+          [
+            '<h1>Foo</h1>',
+            '<div><pre><code class="language-js">const foo = \'bar\'',
+            '</code></pre></div>'
+          ].join('\n'),
         'should match with wrap dive and selector pre'
-      )
+      );
 
       const ast = unified()
         .use(remarkParse)
         .use(remarkRehype)
-        .use(wrap, {wrap: 'div'})
-        .parse('<h1>foo</h1>')
+        .use(wrap, { wrap: 'div' })
+        .parse('<h1>foo</h1>');
 
-      t.ok(vfile, ast.type)
-    })
-    it.end()
-  })
-  t.end()
-})
+      t.ok(vfile, ast.type);
+    });
+    it.end();
+  });
+  t.end();
+});
